78591212c941009a09b1f4441baf0280b6bd32f6
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-05-15 17:57:53 -0600

    Add submodule glfw

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "Adder/adder.h"
+#include "Trim/trim.h"
 
 int main() {
     std::cout << "Hey Zeus!" << std::endl;
 
     std::cout << add(72.1f, 73.8f) << std::endl;
+    std::cout << trim("jakbkdjdn kdmlkmsdmd", ' ') << std::endl;
 
     return 0;
 
